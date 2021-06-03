7f9ed56c4f11329ca662ca9a208753578344c7fc
unknown <gltudor96@gmail.com>
2021-06-03 14:48:19 +0300

    search vue

---
+++ src/components/SearchItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <form action="">
+    <div class="form-group">
+      <label for="exampleFormControlInput1">Search</label>
+      <input
+        type="text"
+        class="form-control"
+        id="exampleFormControlInput1"
+        placeholder="name@example.com"
+        v-model="input"
+      />
+      <button type="submit" class="btn btn-primary">Sign in</button>
+    </div>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    const searchInput = ref('');
+
+    return {
+      input: searchInput,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
